fb0c00b819bbc7cf188d6a7a2f3f857eb72ada7b
peter <a1871898@adelaide.edu.au>
2023-11-12 23:59:46 +1030

    test4

--- prac7-python/CompilerParser.py
+++ prac7-python/CompilerParser.py
@@ -1,77 +1,290 @@
-class CompilerParser:
-    def __init__(self, tokens):
-        self.tokens = tokens
-        self.current_token_index = 0
+from ParseTree import *
 
-    # ... (other methods)
+class CompilerParser:
+
+    def __init__(self,tokens):
+        """
+        Constructor for the CompilerParser
+        @param tokens A list of tokens to be parsed
+        """
+        self.tokens = tokens
+        self.currentTokenIndex = 0
+
+    
 
     def compileProgram(self):
+        """
+        Generates a parse tree for a single program
+        @return a ParseTree that represents the program
+        """
         parse_tree = ParseTree("program")
-        while self.current_token_index < len(self.tokens):
-            parse_tree.children.append(self.compileClass())
-        return parse_tree
-
+        while self.currentTokenIndex <len(self.tokens):
+            parse_tree.addChild(self.compileClass()) 
+        return parse_tree 
+    
+    
     def compileClass(self):
+        """
+        Generates a parse tree for a single class
+        @return a ParseTree that represents a class
+        """
         parse_tree = ParseTree("class")
-        parse_tree.children.append(self.mustBe("KEYWORD", "class"))
-        parse_tree.children.append(self.mustBe("IDENTIFIER"))
-        parse_tree.children.append(self.mustBe("SYMBOL", "{"))
-
-        while self.have("KEYWORD", ["static", "field"]):
-            parse_tree.children.append(self.compileClassVarDec())
-
-        parse_tree.children.append(self.mustBe("SYMBOL", "}"))
-        return parse_tree
+        parse_tree.addChild(self.mustBe("keyword","class"))
+        parse_tree.addChild(self.mustBe("identifier",None))
+        parse_tree.addChild(self.mustBe("symbol","{"))
+        
+        while self.have("keyword",["static","field"]):
+            parse_tree.addChild(self.compileClassVarDec())
+        
+        parse_tree.addChild(self.mustBe("symbol","}"))
+        
+        return parse_tree 
+    
 
     def compileClassVarDec(self):
+        """
+        Generates a parse tree for a static variable declaration or field declaration
+        @return a ParseTree that represents a static variable declaration or field declaration
+        """
         parse_tree = ParseTree("classVarDec")
-        parse_tree.children.append(self.mustBe("KEYWORD", ["static", "field"]))
-        parse_tree.children.append(self.mustBe(["KEYWORD", "IDENTIFIER"]))  # Type
-        parse_tree.children.append(self.mustBe("IDENTIFIER"))  # VarName
+        parse_tree.addChild(self.mustBe("keyword",["static","field"]))
+        parse_tree.addChild(self.compileType())
+        parse_tree.addChild(self.mustBe("identifier",None))
 
-        while self.have("SYMBOL", ","):
-            parse_tree.children.append(self.mustBe("SYMBOL", ","))
-            parse_tree.children.append(self.mustBe("IDENTIFIER"))  # VarName
+        while self.have("symbol",","):
+            parse_tree.addChild(self.mustBe("symbol",","))
+            parse_tree.addChild(self.mustBe("identifier",None))
+
+        parse_tree.addChild(self.mustBe("symbol",";"))
+        return parse_tree 
+    
+
+    def compileSubroutine(self):
+        """
+        Generates a parse tree for a method, function, or constructor
+        @return a ParseTree that represents the method, function, or constructor
+        """
+        return None 
+    
+    
+    def compileParameterList(self):
+        """
+        Generates a parse tree for a subroutine's parameters
+        @return a ParseTree that represents a subroutine's parameters
+        """
+        return None 
+    
+    
+    def compileSubroutineBody(self):
+        """
+        Generates a parse tree for a subroutine's body
+        @return a ParseTree that represents a subroutine's body
+        """
+        return None 
+    
+    
+    def compileVarDec(self):
+        """
+        Generates a parse tree for a variable declaration
+        @return a ParseTree that represents a var declaration
+        """
+        return None 
+    
+
+    def compileStatements(self):
+        """
+        Generates a parse tree for a series of statements
+        @return a ParseTree that represents the series of statements
+        """
+        parse_tree = ParseTree("statements")
+
+        while self.have("KEYWORD", ["let", "do", "if", "while", "return"]):
+            if self.have("KEYWORD", "let"):
+                parse_tree.children.append(self.compileLet())
+            elif self.have("KEYWORD", "do"):
+                parse_tree.children.append(self.compileDo())
+            elif self.have("KEYWORD", "if"):
+                parse_tree.children.append(self.compileIf())
+            elif self.have("KEYWORD", "while"):
+                parse_tree.children.append(self.compileWhile())
+            elif self.have("KEYWORD", "return"):
+                parse_tree.children.append(self.compileReturn())
+
+        return parse_tree
+    
+    
+    def compileLet(self):
+        """
+        Generates a parse tree for a let statement
+        @return a ParseTree that represents the statement
+        """
+        parse_tree = ParseTree("letStatement")
+        parse_tree.children.append(self.mustBe("KEYWORD", "let"))
+        parse_tree.children.append(self.mustBe("IDENTIFIER"))  # varName
+        parse_tree.children.append(self.mustBe("SYMBOL", "="))
+
+        # Partial implementation of compileExpression
+        parse_tree.children.append(self.compileExpression())
 
         parse_tree.children.append(self.mustBe("SYMBOL", ";"))
         return parse_tree
 
-    # Implement other methods as per the given structure
+
+    def compileIf(self):
+        """
+        Generates a parse tree for an if statement
+        @return a ParseTree that represents the statement
+        """
+        parse_tree = ParseTree("ifStatement")
+        parse_tree.children.append(self.mustBe("KEYWORD", "if"))
+        parse_tree.children.append(self.mustBe("SYMBOL", "("))
+
+        # Partial implementation of compileExpression
+        parse_tree.children.append(self.compileExpression())
+
+        parse_tree.children.append(self.mustBe("SYMBOL", ")"))
+        parse_tree.children.append(self.mustBe("SYMBOL", "{"))
+
+        # Partial implementation of compileStatements
+        parse_tree.children.append(self.compileStatements())
+
+        parse_tree.children.append(self.mustBe("SYMBOL", "}"))
+
+        if self.have("KEYWORD", "else"):
+            parse_tree.children.append(self.mustBe("KEYWORD", "else"))
+            parse_tree.children.append(self.mustBe("SYMBOL", "{"))
+
+            # Partial implementation of compileStatements
+            parse_tree.children.append(self.compileStatements())
+
+            parse_tree.children.append(self.mustBe("SYMBOL", "}"))
+
+        return parse_tree
+
+    
+    def compileWhile(self):
+        """
+        Generates a parse tree for a while statement
+        @return a ParseTree that represents the statement
+        """
+        parse_tree = ParseTree("whileStatement")
+        parse_tree.children.append(self.mustBe("KEYWORD", "while"))
+        parse_tree.children.append(self.mustBe("SYMBOL", "("))
+
+        # Partial implementation of compileExpression
+        parse_tree.children.append(self.compileExpression())
+
+        parse_tree.children.append(self.mustBe("SYMBOL", ")"))
+        parse_tree.children.append(self.mustBe("SYMBOL", "{"))
+
+        # Partial implementation of compileStatements
+        parse_tree.children.append(self.compileStatements())
+
+        parse_tree.children.append(self.mustBe("SYMBOL", "}"))
+        return parse_tree
+
+
+    def compileDo(self):
+        """
+        Generates a parse tree for a do statement
+        @return a ParseTree that represents the statement
+        """
+        return None 
+
+
+    def compileReturn(self):
+        """
+        Generates a parse tree for a return statement
+        @return a ParseTree that represents the statement
+        """
+        return None 
+
+
+    def compileExpression(self):
+        """
+        Generates a parse tree for an expression
+        @return a ParseTree that represents the expression
+        """
+        return None 
+
+
+    def compileTerm(self):
+        """
+        Generates a parse tree for an expression term
+        @return a ParseTree that represents the expression term
+        """
+        return None 
+
+
+    def compileExpressionList(self):
+        """
+        Generates a parse tree for an expression list
+        @return a ParseTree that represents the expression list
+        """
+        return None 
+
 
     def next(self):
-        if self.current_token_index < len(self.tokens) - 1:
-            self.current_token_index += 1
+        """
+        Advance to the next token
+        """
+        if self.currentTokenIndex < len(self.tokens):
+            self.currentTokenIndex += 1
         else:
-            raise ParseException("Cannot advance further, end of input reached")
+            raise ParseException("No more tokens")
+
 
     def current(self):
-        return self.tokens[self.current_token_index]
+        """
+        Return the current token
+        @return the token
+        """
+        return self.tokens[self.currentTokenIndex]
 
-    def have(self, token_type, expected_values=None):
-        current_token = self.current()
-        if current_token.token_type == token_type:
-            if expected_values is None or current_token.value in expected_values:
+
+    def have(self,expectedType,expectedValue):
+        """
+        Check if the current token matches the expected type and value.
+        @return True if a match, False otherwise
+        """
+        currentToken = self.current()
+        if currentToken.type == expectedType:
+            if expectedValue == None:
+                return True
+            elif currentToken.value == expectedValue:
                 return True
         return False
 
-    def mustBe(self, token_type, expected_values=None):
-        if self.have(token_type, expected_values):
-            current_token = self.current()
-            self.next()
-            return current_token
-        else:
-            raise ParseException(f"Expected token type: {token_type}, value: {expected_values}, but found {self.current().token_type}, value: {self.current().value}")
 
+    def mustBe(self,expectedType,expectedValue):
+        """
+        Check if the current token matches the expected type and value.
+        If so, advance to the next token, returning the current token, otherwise throw/raise a ParseException.
+        @return token that was current prior to advancing.
+        """
+        if self.have(expectedType,expectedValue):
+            currentToken = self.current()
+            self.next()
+            return currentToken
+        else:
+            raise ParseException("Expected token not found")
+        
+    
 
 if __name__ == "__main__":
-    # ... (other code)
 
-    tokens = [
-        Token("KEYWORD", "class"),
-        Token("IDENTIFIER", "MyClass"),
-        Token("SYMBOL", "{"),
-        Token("SYMBOL", "}"),
-    ]
+
+    """ 
+    Tokens for:
+        class MyClass {
+        
+        }
+    """
+    tokens = []
+    tokens.append(Token("keyword","class"))
+    tokens.append(Token("identifier","MyClass"))
+    tokens.append(Token("symbol","{"))
+    tokens.append(Token("symbol","}"))
 
     parser = CompilerParser(tokens)
     try:
@@ -79,3 +292,5 @@ if __name__ == "__main__":
         print(result)
     except ParseException:
         print("Error Parsing!")
+
+
